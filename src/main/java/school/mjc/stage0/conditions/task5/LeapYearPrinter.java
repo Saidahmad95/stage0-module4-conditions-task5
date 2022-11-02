9ddf032c443370bf885a633602bb646a2abb802d
Saidahmad95 <murtazoyev00@gmail.com>
2022-11-02 13:40:08 +0500

    first commit

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -1,6 +1,9 @@
 package school.mjc.stage0.conditions.task5;
 
+import java.time.Year;
+
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        System.out.println(((year & 3) == 0) && ((year % 100) != 0 || (year % 400) == 0) ? "leap" : "not leap");
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -1,6 +1,50 @@
 package school.mjc.stage0.conditions.task5;
 
+import java.time.LocalDate;
+import java.time.Month;
+
 public class SwitchDaysInMonthPrinter {
     public void amountOfDays(int month) {
+
+        switch (month) {
+            case 1:
+                System.out.println(Month.of(1).length(false));
+                break;
+            case 2:
+                System.out.println(Month.of(2).length(false));
+                break;
+            case 3:
+                System.out.println(Month.of(3).length(false));
+                break;
+            case 4:
+                System.out.println(Month.of(4).length(false));
+                break;
+            case 5:
+                System.out.println(Month.of(5).length(false));
+                break;
+            case 6:
+                System.out.println(Month.of(6).length(false));
+                break;
+            case 7:
+                System.out.println(Month.of(7).length(false));
+                break;
+            case 8:
+                System.out.println(Month.of(8).length(false));
+                break;
+            case 9:
+                System.out.println(Month.of(9).length(false));
+                break;
+            case 10:
+                System.out.println(Month.of(10).length(false));
+                break;
+            case 11:
+                System.out.println(Month.of(11).length(false));
+                break;
+            case 12:
+                System.out.println(Month.of(12).length(false));
+                break;
+            default:
+                System.out.println("wrong number!");
+        }
     }
 }
